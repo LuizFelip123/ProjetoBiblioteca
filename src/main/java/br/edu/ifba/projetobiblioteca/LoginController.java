2ce45d3c60339b22a177c5a1f7cb72334c838c44
Luiz Felipe <85415721+LuizFelip123@users.noreply.github.com>
2022-09-29 16:19:17 -0300

    btn-close add

--- src/main/java/br/edu/ifba/projetobiblioteca/LoginController.java
+++ src/main/java/br/edu/ifba/projetobiblioteca/LoginController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -12,10 +13,15 @@ public class LoginController implements Initializable {
     
  
     @FXML
-    private ImageView closeBtn;
+    private Button closeBtn;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     } 
    
+    @FXML
+    void close(ActionEvent event) {
+        System.exit(0);
+    }
+
 }
